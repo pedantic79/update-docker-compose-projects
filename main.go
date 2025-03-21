7a7a615b98e010df9d0a62e948b54143edc2eb77
Dan Nguyen <pedantic@gmail.com>
2025-03-21 08:50:52 -0500

    Add comments, and reorganize

--- main.go
+++ main.go
@@ -22,19 +22,8 @@ func quit(err error) {
 	panic(err)
 }
 
-func fileExists(filename string) bool {
-	_, err := os.Stat(filename)
-	return err == nil || !os.IsNotExist(err)
-}
-
-func GetImageID(imageName string) (string, error) {
-	// Create a new Docker client
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-	if err != nil {
-		return "", err
-	}
-	defer cli.Close()
-
+// GetImageID returns the ImageID via the docker API. Pass it the full image with tag
+func GetImageID(cli *client.Client, imageName string) (string, error) {
 	imageInspect, err := cli.ImageInspect(context.Background(), imageName)
 	if err != nil {
 		return "", err
@@ -43,11 +32,8 @@ func GetImageID(imageName string) (string, error) {
 	return imageInspect.ID, nil
 }
 
-func loadProject(ctx context.Context, projectName string, workingDir string) (*types.Project, error) {
-	configFile := filepath.Join(workingDir, "docker-compose.yml")
-	if !fileExists(configFile) {
-		configFile = filepath.Join(workingDir, "docker-compose.yaml")
-	}
+func loadProject(ctx context.Context, projectName string, configFile string) (*types.Project, error) {
+	workingDir := filepath.Dir(configFile)
 
 	options := &cli.ProjectOptions{
 		WorkingDir:  workingDir,
@@ -63,44 +49,66 @@ func loadProject(ctx context.Context, projectName string, workingDir string) (*t
 	return project, nil
 }
 
-func main() {
-	ctx := context.Background()
-
+// Wrap creation of compse service
+func newDockerComposeService() api.Service {
 	dockerCli, err := command.NewDockerCli()
 	if err != nil {
 		quit(err)
 	}
+
 	if err := dockerCli.Initialize(flags.NewClientOptions()); err != nil {
 		quit(err)
 	}
 
-	backend := compose.NewComposeService(dockerCli)
-	sv, err := backend.List(ctx, api.ListOptions{All: true})
+	return compose.NewComposeService(dockerCli)
+}
+
+func main() {
+	ctx := context.Background()
+
+	// Create a new Docker client
+	dockerClient, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		quit(err)
+	}
+	defer dockerClient.Close()
+
+	service := newDockerComposeService()
+	sv, err := service.List(ctx, api.ListOptions{All: true})
 	if err != nil {
 		quit(err)
 	}
 
 	for _, view := range sv {
 		projectName := view.Name
+
+		// only include running projects
 		if !strings.HasPrefix(view.Status, "running(") {
 			fmt.Fprintln(os.Stderr, "skipping:", projectName, view.Status)
 			continue
 		}
-		images, err := backend.Images(ctx, projectName, api.ImagesOptions{})
-		if err != nil {
-			quit(err)
-		}
 
-		project, err := loadProject(ctx, projectName, filepath.Dir(view.ConfigFiles))
+		// load the project information for future commands
+		project, err := loadProject(ctx, projectName, view.ConfigFiles)
 		if err != nil {
 			fmt.Println(project, view.ConfigFiles, filepath.Dir(view.ConfigFiles))
 			quit(err)
 		}
 
+		// get Images for the project
+		images, err := service.Images(ctx, projectName, api.ImagesOptions{})
+		if err != nil {
+			quit(err)
+		}
+
+		// Loop through the images
 		needsRestart := false
-		for _, image := range images {
+		for i := range images {
+			image := &images[i]
 			fmt.Println(view.Name, image.Repository, image.ID)
-			err := backend.Pull(ctx, project, api.PullOptions{
+
+			// Attempt a image pull on the image
+			err := service.Pull(ctx, project, api.PullOptions{
 				Quiet:           false,
 				IgnoreFailures:  false,
 				IgnoreBuildable: true,
@@ -109,8 +117,9 @@ func main() {
 				quit(err)
 			}
 
+			// Check if the imageID has changed
 			imageWithTag := fmt.Sprintf("%s:%s", image.Repository, image.Tag)
-			currentImageID, err := GetImageID(imageWithTag)
+			currentImageID, err := GetImageID(dockerClient, imageWithTag)
 			if err != nil {
 				quit(err)
 			}
@@ -121,13 +130,18 @@ func main() {
 			}
 		}
 
+		// If any of the images have been updated, then restart the project
 		if needsRestart {
+			// We're trying to replicate this command
+			// docker compose up --force-recreate --build --remove-orphans --pull always -d
+			// we're ignoring the pull here, because we've already pulled images previously
 			upOpts := api.UpOptions{
 				Create: api.CreateOptions{
 					Build:         &api.BuildOptions{},
 					RemoveOrphans: true,
 					Recreate:      api.RecreateForce,
 				},
+				// this specifies which services to start, we always want all of them
 				Start: api.StartOptions{
 					Project:  project,
 					Services: project.ServiceNames(),
@@ -135,7 +149,7 @@ func main() {
 			}
 
 			fmt.Println("Restarting project:", projectName)
-			if err := backend.Up(ctx, project, upOpts); err != nil {
+			if err := service.Up(ctx, project, upOpts); err != nil {
 				quit(err)
 			}
 		}
